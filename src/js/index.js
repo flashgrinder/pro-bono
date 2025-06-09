9bfc9a583af0da0c28a906b3f2ada8a1935f21cd
flashgrinder <flashgrinder92@gmail.com>
2025-06-09 20:34:16 +0400

    make council page/wip results page

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,14 @@ import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import mobileNav from './modules/mobile-nav.js';
 import mainScreenSliderInit from "./modules/main-screen-slider";
 import newsSliderInit from "./modules/news-slider";
+import expertsItemsAnimation from "./modules/animation-experts-items";
 
 document.addEventListener('DOMContentLoaded', ()=> {
 
     mobileNav();
     mainScreenSliderInit();
     newsSliderInit();
+    expertsItemsAnimation();
     Fancybox.bind('[data-fancybox]');
 
 })
---
+++ src/js/modules/animation-experts-items.js
@@ -0,0 +1,32 @@
+function expertsItemsAnimation(container = document) {
+
+    let expertsItems = Array.from(document.querySelectorAll('.js-expert-item'));
+
+    if(expertsItems) {
+
+        expertsItems.forEach((el, i) => {
+            el.addEventListener("click", function () {
+
+                const currentlySelectedElem = document.querySelector(".js-expert-item.is-active");
+                if (currentlySelectedElem) {
+                    currentlySelectedElem.classList.remove("is-active");
+                }
+
+                el.classList.toggle("is-active");
+                event.stopPropagation();
+            });
+        })
+
+        const clickOutside = e => {
+            const currentlySelectedElem = document.querySelector(".js-expert-item.is-active");
+            if (currentlySelectedElem) {
+                currentlySelectedElem.classList.remove("is-active");
+            }
+        }
+
+        window.addEventListener("click", clickOutside, false);
+
+    }
+}
+
+export default expertsItemsAnimation;
